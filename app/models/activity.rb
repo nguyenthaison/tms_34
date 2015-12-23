2822d170cd5c47a688bf53d4acb2b4ffea34104e
SonNT91 <nguyenthaison9193@gmail.com>
2015-12-23 10:28:30 +0700

    update view activity and frontend

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,14 +1,14 @@
 class Activity < ActiveRecord::Base
   include Rails.application.routes.url_helpers
-  ACTION = {FINISH_SUBJECT: "SUBJECT"}
+  ACTION = {FINISH_SUBJECT: "finish_subject"}
   belongs_to :user
 
-  scope :activities_of_course, -> type_id {where "action_type LIKE '%COURSE' AND type_id = ?", type_id}
-  scope :activities_of_subject, -> type_id {where "action_type LIKE '%SUBJECT' AND type_id = ?", type_id}
-  scope :type_id_of_user, -> user_id {where "user_id = ? AND action_type LIKE '%SUBJECT'", user_id}
+  scope :activities_of_course, -> type_id {where "action_type LIKE '%course' AND type_id = ?", type_id}
+  scope :activities_of_subject, -> type_id {where "action_type LIKE '%subject' AND type_id = ?", type_id}
+  scope :type_id_of_user, -> user_id {where "user_id = ? AND action_type LIKE '%subject'", user_id}
 
   private
-  def self.add_activity action_type, type_id, user_id, description
-    Activity.create action_type: action_type, type_id: type_id, user_id: user_id, description: description
+  def self.add_activity action_type, type_id, user_id
+    Activity.create action_type: action_type, type_id: type_id, user_id: user_id
   end
 end
